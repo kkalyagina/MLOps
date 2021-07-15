e766e7c57df57abf36f855c8310c286a3540f8c4
Kristina Kaliagina <76058935+kkalyagina@users.noreply.github.com>
2021-07-15 13:26:30 +0300

    Delete Desktop/MLE/Linux/my_package directory

--- Desktop/MLE/Linux/my_package/my_package/__init__.py
+++
@@ -1,2 +0,0 @@
-from .script import get_df
-from .script import plot_7d
--- Desktop/MLE/Linux/my_package/my_package/command_line.py
+++
@@ -1,5 +0,0 @@
-import my_package
-
-def main():
-    print (my_package.get_df("AAPL", "7d"))
-    print(my_package.plot_7d(my_package.get_df("AAPL", "7d")))
